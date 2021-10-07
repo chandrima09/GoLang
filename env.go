d683808b71758cfebd6dda6d005be2d74c411c2a
Chandrima Chakraborty <45023825+chandrima09@users.noreply.github.com>
2021-10-07 19:46:15 -0400

    Add comments
    Added comments to show the question.

--- env.go
+++ env.go
@@ -1,3 +1,11 @@
+//1. env(1)
+//   Create a program which lists environment variables as '=' delimited
+//   key/value pairs:
+//```
+//PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+//HOSTNAME=e6a0557d8a1b
+//```
+
 package main
 
 import (
